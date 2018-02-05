8b37c1eb5a7fc7d10fcfd6b1b3badbaaede8772e
Vitor Paisante <vmpaisante@smarttbot.com>
2018-02-05 00:37:13 -0200

    NEW: added log4j2 dependency for loging throughout the application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,7 @@
 package webencoder;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +14,7 @@ import webencoder.storage.StorageService;
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class Application {
+    public static final Logger logger = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
